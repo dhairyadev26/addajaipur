ab806480b4eb2f880189b33c06f3ddd8d9e4b6a4
Dhruvi Topiya <topiya.dhruvi@gmail.com>
2025-01-23 18:03:20 +0530

    aboutus page
    styling

--- frontend/src/pages/Aboutus.jsx
+++ frontend/src/pages/Aboutus.jsx
@@ -13,25 +13,30 @@ const Aboutus = () => {
           <h3>Keshav Shukla</h3>
           <p>Founder of ADAA Jaipur</p>
         </div>
+        <div></div><div></div><div></div>
         <div className="about-card">
         <img src={cofounder} alt="Cofounder" />
           <h3>Tulsi Prasad Shukla</h3>
-          <p>Current Manager of ADAA Jaipur</p>
+          <p>Co-Founder of ADAA Jaipur</p>
         </div>
       </div>
       <div className="about-right">
-        <h2>About Us</h2>
+        <h1>About Us</h1>
         <p>
           ADAA JAIPUR was started by Keshav Shukla in 2010 and is now managed by
-          his elder son Tulsi Prasad Shukla. It is much reckoned for its
-          in-house exclusive Feminine brand “ADAA”. Adaa has almost all types
-          of collections that an Indian Woman needs, be it Kurties, Plazzos,
-          Gowns, Sharara, and many more.
+          his elder son Tulsi Prasad Shukla. 
+        </p>
+        <p>
+          It is much reckoned for its in-house exclusive Feminine brand “ADAA”. 
+        </p>
+        <p>  
+          Adaa has almost all types of collections that an Indian Woman needs, be it Kurties, Plazzos, Gowns, Sharara, and many more.
         </p>
         <p>
           Our mission is to provide the best quality product at its best price.
-          And we love to read your feedback as it encourages us to be more
-          productive to our lovable customers.
+        </p>
+        <p>
+          And we love to read your feedback as it encourages us to be more productive to our lovable customers.
         </p>
         <h3>"STYLE YOURSELF WITH ADAA!"</h3>
       </div>
